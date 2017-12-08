0281373329edb92b8cbebc46ae41bce3f3651b26
bleitzel <bleitzel@me.com>
2017-12-08 10:07:34 -0500

    final

---
+++ ArrayListSorter.java
@@ -0,0 +1,23 @@
+package org.pltw.examples.triptracker;
+
+import android.util.Log;
+import java.util.ArrayList;
+
+public class ArrayListSorter {
+
+    public static void insertionSort(ArrayList list) {
+
+        for (int j = 1; j < list.size(); j++) {
+            Comparable temp = (Comparable)list.get(j);
+            int possibleIndex = j;
+
+            while (possibleIndex > 0 && temp.compareTo(list.get(possibleIndex - 1)) < 0) {
+                list.set(possibleIndex, list.get(possibleIndex - 1));
+                possibleIndex--;
+            }
+            list.set(possibleIndex, temp);
+
+        }
+        Log.i("Sorter", "Array Sorted");
+    }
+}
